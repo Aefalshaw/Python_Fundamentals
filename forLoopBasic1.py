e09d9947e14d92fa5a5335e42626bcca7b8f9042
Aly Falshaw <56277740+Aefalshaw@users.noreply.github.com>
2020-12-03 14:03:40 -0800

    Update forLoopBasic1.py

--- forLoopBasic1.py
+++ forLoopBasic1.py
@@ -11,19 +11,22 @@
 # Learn how to use basic for loop statements in Python
 # Practice some basic algorithms in Python
 
-# 1 Print all integers from 0 to 150.
+# 1 
+# Print all integers from 0 to 150.
 
 for a in range(151):
     print(a)
 
 
-# 2 Print all the multiples of 5 from 5 to 1,000.
+# 2 
+# Print all the multiples of 5 from 5 to 1,000.
 
 for b in range(5, 1001, 5):
     print(b)
 
 
-# 3 Print integers 1 to 100. If divisible by 5, print "Divisible by 5" instead. If divisible by 10, print "Divisible by 10".
+# 3 
+# Print integers 1 to 100. If divisible by 5, print "Divisible by 5" instead. If divisible by 10, print "Divisible by 10".
 
 for c in range(1, 101):
     if c % 10 == 0:
@@ -34,7 +37,8 @@ for c in range(1, 101):
         print(c)
 
 
-# 4 Add odd integers from 0 to 500,000, and print the final sum.
+# 4 
+# Add odd integers from 0 to 500,000, and print the final sum.
 
 sum = 0
 for d in range(500001):
@@ -43,13 +47,15 @@ for d in range(500001):
 print(sum)
 
 
-# 5 Print positive numbers starting at 2018, counting down by fours.
+# 5 
+# Print positive numbers starting at 2018, counting down by fours.
 
 for e in range(2018, 0, -4):
     print(e)
 
 
-# 6 Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult.
+# 6 
+# Set three variables: lowNum, highNum, mult. Starting at lowNum and going through highNum, print only the integers that are a multiple of mult.
 # For example, if lowNum=2, highNum=9, and mult=3, the loop should print 3, 6, 9 (on successive lines)
 
 lowNum = 2
